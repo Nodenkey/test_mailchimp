ceb2e1cd4050ab221f78daaae0d60993fae72935
Nodenkey <nodenkey@gmail.com>
2020-10-29 12:29:50 +0000

    Connect to mailchimp

--- server/app.js
+++ server/app.js
@@ -15,9 +15,49 @@ app.use(express.json());
 
 // Signup route
 app.post('/signup', (req, res) => {
+    const {firstName, lastName, email} = req.body;
     console.log(req.body);
-    // console.log(firstName);
-   res.send(req.body);
+
+    const dc = 'us2';
+    const api_key = '58dd83d33f7108aa7269ca717a9cc706-us2';
+    const id = 'fe8c09514b'
+
+    //construct req data
+    const data = {
+        members: [
+            {
+            email_address: email,
+            status: 'subscribed',
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+            }
+        }
+        ]
+    }
+
+    const postData = JSON.stringify(data);
+    
+    const options = {
+        url: `https://${dc}.api.mailchimp.com/3.0/lists/${id}`,
+        method: 'POST',
+        headers: {
+            Authorization: `auth ${api_key}`
+        },
+        body: postData
+    }
+
+    request(options, (err, response, body) => {
+        if(err){
+            res.sendStatus(400).send('Something went wrong');
+        }
+        else if (response.statusCode === 200) {
+            res.sendStatus(200)
+        }
+        else{
+            res.sendStatus(400).send('Something went wrong');
+        }
+    });
 });
 
 const PORT = process.env.PORT || 5000;
